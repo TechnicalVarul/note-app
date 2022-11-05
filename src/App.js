8d1a056a0c9e591799cd67d9aeb93351b73026fb
Varul <varulchourasia180@gmail.com>
2022-11-05 11:55:18 +0530

    "Note"

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import "./App.css";
+
+import Header from "./Components/Header";
+import CreateArea from "./Components/CreateNote";
+import Note from "./Components/Note";
+import Count from "./Components/Count";
+import Footer from "./Components/Footer";
+
+function App(props) {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevValue) => {
+      return [...prevValue, newNote];
+    });
+  }
+
+  function deleteNotes(id) {
+    setNotes((preValue) => {
+      return [...preValue.filter((note, index) => index !== id)];
+    });
+  }
+  return (
+    <div className="App">
+      <Header />
+      <Count
+        count={
+          notes.length === 0
+            ? "Empty"
+            : `Showing ${notes.length} Notes`
+        }
+      />
+      <CreateArea onAdd={addNote} />
+      {notes.map((note, index) => (
+        <Note
+          key={index}
+          id={index}
+          title={note.title}
+          content={note.content}
+          onDelete={deleteNotes}
+        />
+      ))}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Footer(props) {
+  return (
+    <div className="footer">
+      <p>Copyright 	&#169; Varul Chaurasia 2022 </p>
+
+    </div>
+  );
+}
+
+export default Footer;
